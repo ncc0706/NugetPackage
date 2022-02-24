bcb42775c645f4dafc0e4e3975b9d36f0886b3d7
niuyuxian <ncc0706@gmail.com>
2022-02-24 09:18:24 +0800

    测试引用其它包

--- NiuYuxian.NetFramework472/HelloWorld.cs
+++ NiuYuxian.NetFramework472/HelloWorld.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace NiuYuxian.NetFramework472
 {
@@ -7,6 +10,25 @@ namespace NiuYuxian.NetFramework472
         public static void SayHello()
         {
             Console.WriteLine("Hello nuget .net framework4.7.2");
+            var md5 = MD5.Create();
+            var password = "abc123";
+            var bytes = md5.ComputeHash(Encoding.UTF8.GetBytes(password));
+            password = BitConverter.ToString(bytes);
+            var user = new User
+            {
+                Password = password,
+                Username = "admin",
+                Age = 10
+            };
+            var jsonData = JsonConvert.SerializeObject(user, Formatting.Indented);
+            Console.WriteLine(jsonData);
         }
+    }
+
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
     }
 }
